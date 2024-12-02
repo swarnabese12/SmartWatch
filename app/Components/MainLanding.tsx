aa034d086322a9563bf69610ae1356abcc492295
Swarna Bese <lakshmibese@gmail.com>
2024-12-02 20:11:20 +0530

    Update MainLanding.tsx
    Update MainLanding.tsx

--- app/Components/MainLanding.tsx
+++ app/Components/MainLanding.tsx
@@ -1,11 +1,9 @@
-// components/MainLanding.tsx
 import Image from "next/image";
 import watchImg from "./smart-watch-image.jpg";
 
 const MainLanding = () => {
   return (
     <div className="flex items-center justify-between container">
-      {/* Left Part: Text with 60% width */}
       <div className="w-3/5 p-12">
         <h2 className="text-2xl">New Arrivals</h2>
         <h1 className="text-5xl font-bold text-green mt-4">Smart Watch</h1>
@@ -24,14 +22,13 @@ const MainLanding = () => {
         </button>
       </div>
 
-      {/* Right Part: Image with 40% width */}
       <div className="w-2/5 flex justify-start p-8y">
         <div className="bg-light-grey p-4">
           <Image
             src={watchImg}
             alt="Smartwatch"
-            width={520} // Adjust width as needed
-            height={630} // Adjust height as needed
+            width={520}
+            height={630}
           />
         </div>
       </div>
